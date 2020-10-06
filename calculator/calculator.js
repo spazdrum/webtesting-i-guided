4fb503b031b7715e11a68ebfca49df92de70ad29
spazdrum <spazdrum@gmail.com>
2020-10-05 22:36:47 -0400

    it and it.todo

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,12 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+function add(a = 0, b = 0) {
+  return a + b;
+
+  if (a === b) {
+    return a * 2;
+  } else {
+    return a * b;
+  }
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -2,15 +2,23 @@ const { add } = require("./calculator.js");
 
 // test away!
 
-test("test that truth is trugh", () => {
-  expect(true).toBe(true);
-  // if (true === true) {
-  //     // do some that indicates that the test passed
-  // } else {
-  //     // do something that indicates that it failed
-  // }
-});
+describe("calculator.js tests", () => {
+  describe("add() tests", () => {
+    it("should return the sum of numbers passed", () => {
+      expect(add(2, 2)).toBe(4);
+      expect(add(2, 3)).toBe(5);
+    });
 
-it("should test that true is true", () => {
-  expect(true).toBe(true);
+    it.todo("should return NaN when called with no arguments");
+
+    it("should return 0 when called with no arguments", () => {
+      const expected = 0;
+      const actual = add();
+      expect(actual).toBe(expected);
+    });
+
+    it("should return 0 when called with no parameters", () => {
+      expect(add()).toBe(0);
+    });
+  });
 });
