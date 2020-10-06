da99b767247d10d59e9239b5528701230704cc08
spazdrum <spazdrum@gmail.com>
2020-10-05 22:12:17 -0400

    added trial tests

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,16 @@
-const { add } = require('./calculator.js');
+const { add } = require("./calculator.js");
 
 // test away!
+
+test("test that truth is trugh", () => {
+  expect(true).toBe(true);
+  // if (true === true) {
+  //     // do some that indicates that the test passed
+  // } else {
+  //     // do something that indicates that it failed
+  // }
+});
+
+it("should test that true is true", () => {
+  expect(true).toBe(true);
+});
